561a8158f632cc155060a710b8e3a938e843c8f1
zameel <zameel@pickme.lk>
2022-04-30 09:07:38 +0530

    Changes

--- run.py
+++ run.py
@@ -13,12 +13,12 @@ import subprocess
 scripts = (
 "python3 main_1.py",\
 "python3 main_2.py",\
-"python3 main_3.py","python3 main_4.py","python3 main_5.py","python3 main_6.py")
+"python3 main_3.py","python3 main_4.py")
 
 scripts1 = (
 "/home/halaldeen-ms/dev/DroneSwarm/main_1.py",\
 "/home/halaldeen-ms/dev/DroneSwarm/main_2.py",\
-"/home/halaldeen-ms/dev/DroneSwarm/main_3.py","/home/halaldeen-ms/dev/DroneSwarm/main_4.py", 
+"/home/halaldeen-ms/dev/DroneSwarm/main_3.py","/home/halaldeen-ms/dev/DroneSwarm/main_4.py",
 "/home/halaldeen-ms/dev/DroneSwarm/main_5.py","/home/halaldeen-ms/dev/DroneSwarm/main_6.py")
 
 
